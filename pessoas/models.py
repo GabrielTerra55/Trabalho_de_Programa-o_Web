cea4a2822441b5c162b6ae5f0bc956771c9c9856
GabrielTerra55 <gabrielterra055@gmail.com>
2023-06-27 08:02:13 -0300

    removendo lista de pessoas

--- pessoas/models.py
+++ pessoas/models.py
@@ -4,6 +4,5 @@ from django.db import models
 class Pessoa(models.Model):
 	nome = models.CharField(max_length=200)
 	email = models.CharField(max_length=200)
-	amigos = ArrayField(models.IntegeerField())
 	def __str__(self):
 		return self.nome
