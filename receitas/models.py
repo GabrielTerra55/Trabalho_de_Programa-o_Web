267cfec59b55bac6ac089ae6afaa9f1de35cecc1
hermelan <hermelandavid@gmail.com>
2023-06-26 21:35:26 -0300

    modificar receitas

--- receitas/models.py
+++ receitas/models.py
@@ -1,6 +1,8 @@
 from django.db import models
 from datetime import datetime
+from pessoas.models import Pessoa
 class Receita(models.Model):
+	pessoa = models.ForeignKey(Pessoa, on_delete=models.CASCADE)
 	nome_receita = models.CharField(max_length=200)
 	ingredientes = models.TextField()  
 	modo_preparo = models.TextField()
@@ -8,3 +10,7 @@ class Receita(models.Model):
 	rendimento = models.CharField(max_length=100)
 	categoria = models.CharField(max_length=100)
 	date_categoria = models.DateTimeField(default=datetime.now, blank=True)
+	foto_receita = models.ImageField(upload_to='fotos/%d/%m/%Y/', blank=True)
+	publicada = models.BooleanField(default=False)
+	def __str__(self):
+		return self.nome_receita
