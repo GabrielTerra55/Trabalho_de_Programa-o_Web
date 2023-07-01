845f71d31d2d9dd25a3ec0f3267ded999c702fb4
GabrielTerra55 <gabrielterra055@gmail.com>
2023-07-01 19:26:28 -0300

    remove comand

--- build_files.sh
+++ build_files.sh
@@ -1,4 +1,3 @@
 pip install -r requirements.txt
 python3.9 manage.py collectstatic --noinput
 python3.9 manage.py migrate
-echo "from django.contrib.auth.models import User; User.objects.create_superuser('admin', 'admin@admin.com', '40028922')" | python3.9 manage.py shell
