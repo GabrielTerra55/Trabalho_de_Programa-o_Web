588d93409f2a30586ca07b41826fdca1db43c725
GabrielTerra55 <gabrielterra055@gmail.com>
2023-07-01 17:54:58 -0300

    url patterns

--- djangoreceita/urls.py
+++ djangoreceita/urls.py
@@ -7,4 +7,7 @@ urlpatterns = [
     path('',include('receitas.urls')),
     path('usuarios/',include('usuarios.urls')),
     path('admin/', admin.site.urls),
-] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+]
+
+urlpatterns += static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
+urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
